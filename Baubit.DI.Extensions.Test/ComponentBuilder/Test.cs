3f7f21e690ed8064c8767063e3c48365263ecc67
pnagoorkar <prashant.nagoorkar@gmail.com>
2025-12-02 17:08:51 -0500

    Initial commit

---
+++ Baubit.DI.Extensions.Test/ComponentBuilder/Test.cs
@@ -0,0 +1,10 @@
+﻿namespace Baubit.DI.Extensions.Test.ComponentBuilder
+{
+    /// <summary>
+    /// Unit tests for <see cref="Baubit.DI.Extensions.ComponentBuilder{T}"/>
+    /// </summary>
+    public class Test
+    {
+
+    }
+}
---
+++ Baubit.DI.Extensions/ComponentBuilder.cs
@@ -0,0 +1,21 @@
+﻿using FluentResults;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Baubit.DI.Extensions
+{
+    public class ComponentBuilder<T> : ComponentBuilder where T : class
+    {
+        public new Result<T> Build()
+        {
+            return base.Build().Bind(component => Result.Try(() =>
+            {
+                var services = new ServiceCollection().AddSingleton<T>();
+                foreach (var module in component)
+                {
+                    module.Load(services);
+                }
+                return services.BuildServiceProvider().GetRequiredService<T>();
+            }));
+        }
+    }
+}
